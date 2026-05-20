5d147ee7a100cf2415e44f464d320793ff6c308d
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2026-05-20 18:04:49 +1200

    Remove cygwin scripts and add test_all.bat

--- posix-time/cygwin_build.sh
+++
@@ -1,7 +0,0 @@
-export PATH=/usr/bin:\$PATH
-cd /cygdrive/c/Users/samue/repos/auto-win-msvc/posix-time
-mkdir -p build_cygwin
-cd build_cygwin
-cmake -G "Unix Makefiles" ..
-make
-./test_posix-time.exe
